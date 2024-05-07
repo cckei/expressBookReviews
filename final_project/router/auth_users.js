35d7b400518e922b1a27ab9e1bfa700d85f1ba2f
cckei <cckei.chan@gmail.com>
2024-05-07 17:47:03 -0400

    work in progress

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,19 +11,34 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let user = users.filter((user)=>user.username == username)[0];
+    if(!user) return false;
+    if(user.password == password) return true;
+    return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const {username, password} = req.body;
+
+    // Authenticate the user
+    if (authenticatedUser(username, password)) {
+        const token = jwt.sign({ username }, "your_secret_key");
+        req.session.user = token;
+        return res.status(200).json({ message: "Logged in successfully", token });
+    } else {
+        return res.status(401).json({ message: "Invalid username or password" });
+    }
 });
 
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review", (req,res) => {
+    return res.status(200).json({ message: "review added" });
 });
+regd_users.delete("/login2", (req,res) => {
+    return res.status(200).json({ message: "review deleted" });
+});
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,64 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username, password} = req.body;
+  if(!username) return res.status(400).json({message: "please enter the username"});
+  if(!password) return res.status(400).json({message: "please enter the password"});
+
+  if(users.filter((user)=>user.username == username).length){
+    return res.status(400).json({message: "user exists."});
+  }
+
+  users.push({
+    username: username,
+    password: password
+  })
+  return res.status(200).json({message: "user created."});
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({books: books});
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  var isbn = req.params.isbn;
+  var bookDetails = books[isbn];
+  return res.status(200).json({bookDetails: bookDetails});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+  var author = req.params.author;
+  for(var key in books){
+    if(books[key].author == author){
+        var bookDetails = books[key];
+        return res.status(200).json(bookDetails);  
+    }
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    var title = req.params.title;
+    for(var key in books){
+      if(books[key].title == title){
+          var bookDetails = books[key];
+          return res.status(200).json(bookDetails);  
+      }
+    }
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    var isbn = req.params.isbn;
+    var bookDetails = books[isbn];
+    return res.status(200).json({message: 'review added'});
 });
 
 module.exports.general = public_users;
